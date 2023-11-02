2364cf45f3fcf96283adb358361161c7b1bc0a03
shcho <acckr0604@gmail.com>
2023-11-02 21:44:02 +0900

    view: 클래스 목록 -> 필터링 UI 구현

--- src/app/main/class/page.tsx
+++ src/app/main/class/page.tsx
@@ -1,3 +1,10 @@
+"use client"
+import { useSearchParams } from 'next/navigation';
+import dropDown from '../../../assets/icons/dropDown.svg';
+import upIcon from '../../../assets/icons/upIcon.svg';
+import Image from 'next/image';
+import { useState } from 'react';
+
 export default function ClassPage() {
   const data = [
     {
@@ -26,7 +33,53 @@ export default function ClassPage() {
       userCnt: 50
     }
   ]
+
+  let [clickClassFilter, setClickClassFilter] = useState<boolean>(false);
+
   return (
+    <>
+    <section className="absolute top-[120px] flex w-[343px] h-[37px] gap-2">
+    <div className='flex flex-col w-[125px] gap-2'>
+      {!clickClassFilter ? 
+      <>
+      <button className="flex gap-2 justify-center items-center px-3 py-2 border rounded-lg border-[#7354E8]" onClick={() => {
+        setClickClassFilter(prev => !prev);
+      }}>
+      <span className="text-[#7354E8] text-[14px] font-semibold">클래스 유형</span>
+      <Image src={dropDown} width={16} height={17} alt=''/>
+      </button>
+      </> : 
+      <>
+      <button className="flex gap-2 justify-center items-center px-2 py-2 border-[3px] rounded-lg border-[#D3C4F9] bg-[#563AC0]" onClick={() => {
+        setClickClassFilter(prev => !prev);
+      }}>
+      <span className="text-[#FFF] text-[14px] font-semibold">클래스 유형</span>
+      <Image src={upIcon} width={16} height={17} alt=''/>
+      </button>
+      <div className='flex w-[195px] p-4 rounded-lg border border-[#D1D5DB] bg-[#FFF]'>
+        <div className='w-[100%] flex flex-col gap-3'>
+        <div className='flex'>
+          <input type='radio' id='time_class' value="time_class" />
+          <label htmlFor='time_class'>회차반</label>
+        </div>
+        <div className='flex'>
+          <input type='radio' id="preiod_class" value='preiod_class'/>
+          <label htmlFor='preo[iod_class'>기간반</label>
+        </div>
+        </div>
+      </div>
+      </>}
+    </div>
+    <button className="flex w-auto justify-center items-center px-3 py-2 border rounded-lg border-[#7354E8] bg-[#FFF]">
+    <span className="text-[#7354E8] text-[14px] font-semibold">강사명</span>
+    <Image src={dropDown} width={16} height={17} alt=''/>
+    </button>
+    <button className="flex w-auto justify-center items-center px-3 py-2 border rounded-lg border-[#7354E8] bg-[#FFF]">
+    <span className="text-[#7354E8] text-[14px] font-semibold">카테고리</span>
+    <Image src={dropDown} width={16} height={17} alt=''/>
+    </button>
+    </section>
+    
     <section className="container w-full grid grid-cols-3 gap-[6px]">
       {data.map(({ title, instructor, userCnt }) => (
         <div className=" w-[464px] h-56 px-6 py-8 bg-white rounded-lg shadow border border-gray-200 flex-col justify-start items-start gap-16 inline-flex">
@@ -44,5 +97,6 @@ export default function ClassPage() {
         </div>
       ))}
     </section>
+    </>
   )
 }
