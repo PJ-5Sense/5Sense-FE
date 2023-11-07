3fc5881f02218a44783c8365e2ec2bead49aa7c1
shcho <acckr0604@gmail.com>
2023-11-07 16:35:22 +0900

    view: 클래스 관리 -> 필터링 focus UI 구현

--- src/components/classFilter/classFilter.tsx
+++ src/components/classFilter/classFilter.tsx
@@ -4,18 +4,18 @@ import ChevornDown from '../../assets/icons/chevron-down.svg';
 export default function ClassFilter() {
     return (
         <div className="absolute left-12 top-[120px] w-[310px] h-[37px] items-start gap-2 inline-flex">
-            <div className="group flex items-center gap-2 w-[112px] h-full border px-3 py-2 rounded-lg border-indigo-500 hover:bg-indigo-700">
-                <span className="w-16 h-[21px] text-[11.5px] font-semibold font-['Pretendard'] leading-[21px] text-indigo-500 group-hover:text-white">클래스 유형</span>
+            <button className="group flex items-center gap-2 w-[112px] h-full border px-3 py-2 rounded-lg border-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 focus:outline focus:outline-2.5 focus:outline-[#D3C4F9]">
+                <span className="w-16 h-[21px] text-[11.5px] font-semibold font-['Pretendard'] leading-[21px] text-indigo-500 group-hover:text-white group-focus:text-white">클래스 유형</span>
                 <Image src={ChevornDown} width={16} height={16} alt=" " />
-            </div>
-            <div className="group flex items-center gap-2 w-[85px] h-full px-3 py-2 border rounded-lg border-indigo-500  hover:bg-indigo-700 ">
-                <span className="w-[37px] h-[21px] text-[11.5px] font-semibold font-['Pretendard'] leading-[21px] text-indigo-500 group-hover:text-white">강사명</span>
+            </button>
+            <button className="group flex items-center gap-2 w-[85px] h-full px-3 py-2 border rounded-lg border-indigo-500  hover:bg-indigo-700 focus:bg-indigo-700 focus:outline focus:outline-2.5 focus:outline-[#D3C4F9] focus-within:text-white">
+                <span className="w-[37px] h-[21px] text-[11.5px] font-semibold font-['Pretendard'] leading-[21px] text-indigo-500 group-hover:text-white group-focus:text-white">강사명</span>
                 <Image src={ChevornDown} width={16} height={16} alt=" " />
-            </div>
-            <div className="group flex items-center gap-2 w-[97px] h-full px-3 py-2 border rounded-lg border-indigo-500 hover:bg-indigo-700">
-                <span className="w-[49px] h-[21px] text-[11.5px] font-semibold font-['Pretendard'] leading-[21px] text-indigo-500 group-hover:text-white">카테고리</span>
+            </button>
+            <button className="group flex items-center gap-2 w-[97px] h-full px-3 py-2 border rounded-lg border-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 focus:outline focus:outline-2.5 focus:outline-[#D3C4F9]">
+                <span className="w-[49px] h-[21px] text-[11.5px] font-semibold font-['Pretendard'] leading-[21px] text-indigo-500 group-hover:text-white group-focus:text-white">카테고리</span>
                 <Image src={ChevornDown} width={16} height={16} alt=" " />
-            </div>
+            </button>
         </div>
     )
 }
